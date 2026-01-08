20b56fdde5f9c0e85b7dede0ebdc0d83a69f3fa9
sanyam1323 <saiyamchagtie@gmail.com>
2026-01-08 23:28:47 +0530

    Assignments

---
+++ Second_task.py
@@ -0,0 +1,5 @@
+first_name=input("Enter First Name: ")
+last_name=input("Enter Last Name: ")
+full_name=(first_name + ' ' + last_name)
+print(full_name)
+print("Hello,",full_name,"! Welcome to the python programme")
